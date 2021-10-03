7bfcbde57ee1264cf163ce97f26c92e9ebcc43f0
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-10-03 20:20:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include "mandaw.h" 
+#include "mandaw/mandaw.hpp"
 
 using namespace mandaw;
 
-int main() {
-    Mandaw("Mandaw!", 800, 600, 255, 0, 0).update();
-
-    return 0;
+int main()
+{
+    Mandaw mandaw;
+    mandaw.loop();
 }
